ba23458964612818c93eedf3ac6cf9e24f5e9657
Varun Vachhar <varunvachhar@gmail.com>
2016-01-12 17:11:19 -0500

    See To Do Items - Added grid and card components - Setup the root with mock data

---
+++ app/src/components/grid/grid.ts
@@ -0,0 +1,22 @@
+import {Component, View} from 'angular2/core';
+import Card from '../card/card';
+
+@Component({
+  selector: 'grid',
+  properties: [
+    'items'
+  ]
+})
+@View({
+  directives: [Card],
+  template: `
+    <ul class="cols list-reset m0">
+      <li class="py1 avoid-break"
+        *ngFor="#item of items">
+        <card></card>
+      </li>
+    </ul>
+  `
+})
+export default class Grid {
+}
