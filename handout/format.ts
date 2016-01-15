02b5ee34f9a8cd69950da07c661f2a335ef44df2
Michael J. Bennett <michael.bennett@rangle.io>
2016-01-15 17:11:51 -0500

    (WIP) commiting DI work to avoid catastrophe

---
+++ handout/format.ts
@@ -0,0 +1,19 @@
+function __decorate(decorators, target, key, desc) {
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") {
+      return Reflect.decorate(decorators, target, key, desc);
+    }
+    switch (arguments.length) {
+      case 2:
+        return decorators.reduceRight(function(o, d) {
+          return (d && d(o)) || o;
+        }, target);
+      case 3:
+        return decorators.reduceRight(function(o, d) {
+          return (d && d(target, key)), void 0;
+        }, void 0);
+      case 4:
+        return decorators.reduceRight(function(o, d) {
+          return (d && d(target, key, o)) || o;
+        }, desc);
+    }
+  }
