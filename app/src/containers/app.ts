0c3f87788be42dccbe54ec8512c7b88ba66ddb2a
Gavin Rough <gavinrough@gmail.com>
2016-01-26 10:06:56 -0500

    Require login to access Todo features (in progress)

--- app/src/containers/app.ts
+++ app/src/containers/app.ts
@@ -2,13 +2,15 @@ import {Component} from 'angular2/core';
 import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
 import Header from '../components/header/header';
 import Main from '../components/main/main';
+import Login from '../components/login/login';
 
 @Component({
   selector: 'ngc-root',
-  directives: [ROUTER_DIRECTIVES, Header],
+  directives: [ROUTER_DIRECTIVES, Header, Login],
   template: `
   <ngc-header></ngc-header>
   <div class="container px2 mt4">
+    <login></login>
     <router-outlet></router-outlet>
   </div>
   `
