10901216ac8dfd0b0bfa909ba0767974fc8e92be
Gavin Rough <gavinrough@gmail.com>
2016-01-29 17:20:44 -0500

    Handle login logic in ngOnInit

--- app/src/components/main/main.ts
+++ app/src/components/main/main.ts
@@ -1,11 +1,11 @@
 import {Component, Injector} from 'angular2/core';
-import {RouteConfig, RouterOutlet, CanActivate} from 'angular2/router';
+import {Router, RouteConfig, RouterOutlet} from 'angular2/router';
+import {AuthService} from '../../services/auth-service';
 import TaskAdd from '../task-add/task-add';
 import TaskEdit from '../task-edit/task-edit';
 import TasksList from '../tasks-list/tasks-list';
 import Summary from '../summary/summary';
 import TasksService from '../../services/tasks-service';
-import {AuthService} from '../../services/auth-service';
 
 @Component({
   selector: 'ngc-main',
@@ -29,11 +29,16 @@ import {AuthService} from '../../services/auth-service';
   name: 'TaskEdit',
   component: TaskEdit
 }])
-@CanActivate(
-  (nextInstr: any, currInstr: any) => {
-    let injector: any = Injector.resolveAndCreate([AuthService]);
-    let authService: AuthService = injector.get(AuthService);
-    return authService.isLogged();
+export default class Main {
+
+  constructor(
+    public authService: AuthService,
+    private _router: Router
+  ) {}
+
+  ngOnInit() {
+    if (!this.authService.isLoggedIn()) {
+      this._router.navigate(['/Login']);
+    }
   }
-)
-export default class Main {}
+}
