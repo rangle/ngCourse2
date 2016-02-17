a135c2ab5db331d790dedcb62df52d9752953da7
Michael J. Bennett <michael.bennett@rangle.io>
2016-02-17 18:27:32 -0500

    Adds exercise for string templates

---
+++ exercises/features/es6/1.2-template-strings_solution/app/exercise.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function write(text) {
+  var element = document.getElementById('example');
+  element.innerHTML = text;
+}
+
+var age = 900;
+
+write(`Yoda is ${age} years old`);
+
