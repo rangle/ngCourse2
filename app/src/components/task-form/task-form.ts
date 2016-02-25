d023be37105c658d38b4eb2ea43033f4adcb5bd7
Evan Schultz <evan@rangle.io>
2016-02-25 15:24:59 -0500

    Create task-form dumb component
    * Create task-form dumb component
    * Refactor task-edit to consume component

---
+++ app/src/components/task-form/task-form.ts
@@ -0,0 +1,63 @@
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  ChangeDetectionStrategy,
+  OnInit
+} from 'angular2/core';
+
+import {
+  RouterLink,
+  Router
+} from 'angular2/router';
+
+import {
+  FORM_BINDINGS,
+  FORM_DIRECTIVES,
+  ControlGroup,
+  FormBuilder,
+  Validators
+} from 'angular2/common';
+
+import {
+  Map,
+  fromJS
+} from 'immutable';
+const TEMPLATE = require('./task-form.html');
+
+@Component({
+  selector: 'ngc-task-form',
+  directives: [RouterLink, FORM_DIRECTIVES],
+  viewBindings: [FORM_BINDINGS],
+  template: TEMPLATE,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export default class TaskForm implements OnInit {
+
+  @Input() formTitle: string;
+  @Input() task: Map<string, any> =  Map<string, any>(
+    { _id: undefined, 
+      owner: '',
+      description: '', 
+      done: false });
+  @Output() onSubmit: EventEmitter<any> = new EventEmitter<any>();
+
+
+  taskForm: ControlGroup;
+
+  constructor(private _builder: FormBuilder) {}
+
+  ngOnInit() {
+    this.taskForm = this._builder.group({
+      _id: [this.task.get('_id')],
+      owner: [this.task.get('owner'), Validators.required],
+      description: [this.task.get('description'), Validators.required],
+      done: [this.task.get('done')]
+    });
+  }
+
+  submitTask(taskForm: ControlGroup): void {
+    this.onSubmit.emit(taskForm.value);
+  }
+}
