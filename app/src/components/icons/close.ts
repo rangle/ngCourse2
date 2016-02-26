d8dc5845168b4ab5fea8699a130c703bc399db66
Evan Schultz <evan@rangle.io>
2016-02-26 14:43:27 -0500

    Initial workshop start

--- app/src/components/icons/close.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from 'angular2/core';
-const CLOSE = require('!raw!../../assets/close.svg');
-
-@Component({
-  selector: 'ngc-icon-close',
-  template: CLOSE
-})
-export default class CloseIcon {
-}
--- app/src/components/index.ts
+++ app/src/components/index.ts
@@ -1,1 +1,7 @@
-
+import Container from './container/container';
+import Sample from './sample-component/sample-component';
+import {CheckIcon} from  './icons';
+export {Container, Sample}
+import { CARD_COMPONENTS } from './card';
+export const NGC_ICONS = [CheckIcon]
+export const CARD_COMPONENTS = CARD_COMPONENTS;
