1cebff4ae0936975385e8b70792663511ba7d43f
Evan Schultz <evan@rangle.io>
2016-06-15 12:15:41 -0700

    Fix obs example

--- exercises/observables/1.0-SpotifySearch/app/boot.ts
+++ exercises/observables/1.0-SpotifySearch/app/boot.ts
@@ -1,6 +1,6 @@
 import {bootstrap} from '@angular/platform-browser-dynamic';
 import App from './app-component';
-import {HTTP_BINDINGS} from 'angular2/http';
+import {HTTP_BINDINGS} from '@angular/http';
 import SearchService from './search-service';
 
 bootstrap(App, [HTTP_BINDINGS, SearchService]);
