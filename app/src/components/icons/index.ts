787bdcf2a7da4077a33e7daaf3974e0891c22a94
Andrew Smith <smithad15@gmail.com>
2016-08-26 15:15:24 -0400

    Remove todo app and workshop instructions (#684)

--- app/src/components/icons/index.ts
+++
@@ -1,6 +0,0 @@
-import CheckIcon from '../icons/check';
-import CloseIcon from '../icons/close';
-import EditIcon from '../icons/edit';
-import UserIcon from '../icons/user';
-
-export { CheckIcon , CloseIcon , EditIcon , UserIcon };
--- app/src/components/icons/logo.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from '@angular/core';
-const LOGO = require('!raw!../../assets/logo.svg');
-
-@Component({
-  selector: 'ngc-icon-logo',
-  template: LOGO
-})
-export default class LogoIcon {
-}
