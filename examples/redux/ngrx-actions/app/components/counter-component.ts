9f94a0b0ecfdf17da2981745cac69ce70352888b
Philip Da Silva <philip.dasilva@rangle.io>
2016-09-13 11:13:15 -0400

    Updates ngrx-actions example
    Fix

--- examples/redux/ngrx-actions/app/components/counter-component.ts
+++ examples/redux/ngrx-actions/app/components/counter-component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'counter',
--- examples/redux/ngrx-actions/app/index.ts
+++ examples/redux/ngrx-actions/app/index.ts
@@ -1,11 +1,23 @@
-import { bootstrap } from '@angular/platform-browser-dynamic';
-import { provide } from '@angular/core';
-import { provideStore } from '@ngrx/store';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { StoreModule } from '@ngrx/store';
 
 import { SimpleNgrx } from './containers/app-container';
+import { Counter } from './components/counter-component';
 import { counterReducer } from './reducers/counter-reducer';
 
-bootstrap(SimpleNgrx, [
-  provideStore({ counter: counterReducer })
-]);
-
+@NgModule({
+  imports: [
+    BrowserModule,
+    StoreModule.provideStore({ counter: counterReducer })
+  ],
+  declarations: [
+    SimpleNgrx,
+    Counter
+  ],
+  bootstrap: [ SimpleNgrx ]
+})
+class AppModule {
+}
+platformBrowserDynamic().bootstrapModule(AppModule);
