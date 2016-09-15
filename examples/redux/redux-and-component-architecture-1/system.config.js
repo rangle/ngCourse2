0e17e33209208f7451b613ef47a6e315701bfcef
Philip Da Silva <philip.dasilva@rangle.io>
2016-09-15 16:31:41 -0400

    Updates to 2.0.0 stable

--- examples/redux/redux-and-component-architecture-1/system.config.js
+++ examples/redux/redux-and-component-architecture-1/system.config.js
@@ -1,4 +1,4 @@
-var angularVersion = '2.0.0-rc.6';
+var angularVersion = '2.0.0';
 var rcVersion = '3.0.0-rc.2';
 
 System.config({
@@ -40,10 +40,11 @@ System.config({
     'invariant': 'unpkg:invariant@2.2.1',
     'redux-thunk': 'unpkg:redux-thunk@2.1.0',
     'redux-logger': 'unpkg:redux-logger@2.6.0',
-    'rxjs': 'unpkg:rxjs@5.0.0-beta.11',
-    'zone.js': 'unpkg:zone.js@0.6.17',
+    'rxjs': 'unpkg:rxjs@5.0.0-beta.12',
+    'zone.js': 'unpkg:zone.js@0.6.21',
     'reflect-metadata': 'unpkg:reflect-metadata@0.1.3',
-    "crypto": "@empty"
+    "crypto": '@empty',
+    'timers': '@empty'
   },
 
   packages: {
