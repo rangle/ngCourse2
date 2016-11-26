e514b20677f3573df2ad813f0a2ac6a239ae348f
Rajinder Yadav <rajinder.yadav@hotmail.com>
2016-11-26 15:19:54 -0500

    Plunker code corrected, component name corrected.

--- examples/components/components-input/app/app.component.ts
+++ examples/components/components-input/app/app.component.ts
@@ -7,7 +7,7 @@ import {Component} from '@angular/core';
 	    <hello name="Other World"></hello>
 	  </div>`
 })
-export class App {
+export class AppComponent {
   helloName: string;
 
   constructor() {
